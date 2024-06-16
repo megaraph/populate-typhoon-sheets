4bc220ab577dd9adf07dedc2a65a106a28f1d3d6
megaraph <47447126+megaraph@users.noreply.github.com>
2024-06-17 00:47:14 +0800

    populate correlation before and after

--- means.py
+++ means.py
@@ -8,11 +8,22 @@ WB_PATH = Path.cwd().joinpath(WB_NAME)
 WB = load_workbook(filename=WB_NAME, data_only=True)
 
 
+def main():
+    before_sheets = [sheet for sheet in WB.sheetnames if sheet.startswith("B - ")]
+    populate_corr_sheet(before_sheets, "Correlation Before")
+
+    after_sheets = [sheet for sheet in WB.sheetnames if sheet.startswith("A - ")]
+    populate_corr_sheet(after_sheets, "Correlation After")
+
+
 def indices_results(location, index_dict):
     results = []
     for veg_index in VEG_INDICES:
         values = [row.value for row in location[index_dict[veg_index]]]
         _, *nums = values
+
+        if nums[0] is None:
+            continue
 
         # Perform computation on nums
         result = sum(nums) / len(nums)
@@ -34,31 +45,33 @@ def clear_sheet(sheet_name, headers):
     return new_sheet
 
 
-before_sheets = [sheet for sheet in WB.sheetnames if sheet.startswith("B - ")]
-after_sheets = [sheet for sheet in WB.sheetnames if sheet.startswith("A - ")]
+def populate_corr_sheet(loc_sheets, corr_sheet_name):
+    sample_sheet = WB[loc_sheets[0]]
+    sample_sheet_titles = list(sample_sheet.rows)[
+        0
+    ]  # gets first row in the sample sheet
+
+    # Get titles that are only named "Location" or any of the vegetation indices
+    titles = [
+        title.value
+        for title in sample_sheet_titles
+        if title.value in ["Location", *VEG_INDICES]
+    ]
+
+    corr_sheet = clear_sheet(sheet_name=corr_sheet_name, headers=titles)
+
+    for sheet in loc_sheets:
+        loc = WB[sheet]
+        loc_name = loc["A2"].value
+
+        index_dict = veg_index_dict()
+        results = indices_results(loc, index_dict)
+        row = [loc_name, *results]
+
+        corr_sheet.append(row)
+
+    WB.save(WB_PATH)
 
 
-sample_sheet = WB[before_sheets[0]]
-sample_sheet_titles = list(sample_sheet.rows)[0]  # gets first row in the sample sheet
-
-# Get titles that are only named "Location" or any of the vegetation indices
-titles = [
-    title.value
-    for title in sample_sheet_titles
-    if title.value in ["Location", *VEG_INDICES]
-]
-
-corr_before_sheet = clear_sheet(sheet_name="Correlation Before", headers=titles)
-
-# TODO: REMOVE LIMIT
-for sheet in before_sheets[:2]:
-    loc = WB[sheet]
-    loc_name = loc["A2"].value
-
-    index_dict = veg_index_dict()
-    results = indices_results(loc, index_dict)
-    row = [loc_name, *results]
-
-    corr_before_sheet.append(row)
-
-WB.save(WB_PATH)
+if __name__ == "__main__":
+    main()
